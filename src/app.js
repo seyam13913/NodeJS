50b9a9f5d81652ffad3e58370a43dafd64f6ff0b
seyam13913 <smmatihassen@gmail.com>
2023-12-05 00:49:43 +0600

    http response, html file,json data

--- src/app.js
+++ src/app.js
@@ -3,6 +3,28 @@ const app = express();
 const userRouter = require("./routes/user.route");
 const customer = require("./routes/customer.route");
 
+app.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use("/register", (req, res) => {
+  res.statusCode = 200;
+  res.cookie("name", "SMJubaeair");
+  res.cookie("age", "20");
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+  res.clearCookie("name");
+  res.append("id", "13913");
+  res.send("Hi i'm Login page");
+});
+
+app.use("/logout", (req, res) => {
+  res.send("I'm Logout form my Browser.");
+});
+
 app.use("/api/user/", userRouter);
 
 app.use("/api/user/customer", customer);
