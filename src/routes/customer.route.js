c615ce574d22588c285dc55e01ebcd33b55c1a6f
seyam13913 <smmatihassen@gmail.com>
2023-12-14 11:35:28 +0600

    today larning route header data pass

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const customer = express.Router();
 
+customer.get("/find", (req, res) => {
+  const id = req.header("id");
+  const age = req.header("age");
+  const ok = req.headers;
+  console.log(ok);
+
+  console.log(req.url);
+  console.log(req.baseUrl);
+
+  console.log("userId", id);
+  console.log("userAge", age);
+  res.send(`<h1>Student Id is : ${id}, age is : ${age}</h1>`);
+});
+
 customer.get("/users", (req, res) => {
   res.send("<h2>Hello, I'm new customer.</h2>");
 });
